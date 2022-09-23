53f33221f8d890fe6dad11f698035bfddf041bb0
Anthony England <anthonyengland@Anthonys-MBP.askey.com>
2022-09-23 07:14:48 -0700

    update 2

--- httpServer.js
+++ httpServer.js
@@ -1,17 +1,43 @@
-'use strict';
 
-var http = require('http');
-var pets = require("./pets.json")
-var port = process.env.PORT || 8000;
+const http = require('http');
+const port = process.env.PORT || 8000;
 
-var server = http.createServer(function(req, res) {
- 
-//   var guestsJSON = JSON.stringify(pets.json);
+const server = http.createServer((req, res) => {
+    var pets = require('./pets.js')
+    
+    res.setHeader('Content-Type', 'application/json')
+    for(let i = 0; i < pets.length; i++) {
+        if ( req.method == 'GET' &&req.url == '/pets') {
+            
+          console.log(res.end(JSON.stringify(pets)))
+     
+           
+        }
+        if ( req.method == 'GET' && req.url == '/pets'+ '/' + `${i}`) {
+      
+         res.end(JSON.stringify(pets[i]))
+         break;
+         } else if(req.url == null){
+          throw new error('oops that doesn"t exist')
+         }
+        }})  
 
-  res.setHeader('Content-Type', 'application/json');
-  res.end(pets);
-});
 
-server.listen(port, function() {
-  console.log('Listening on port', port);
+
+//     let url = 
+//     if (req.method === 'GET' && req.url.split() === '/pets') {
+//         var pets = require('./pets.js')
+//         var petsJSON = JSON.stringify(pets)
+//         res.setHeader('Content-Type', 'application/json');
+//         res.end(petsJSON);
+//     }
+//     else {
+//         res.statusCode = 404;
+//         res.setHeader('Content-Type', 'text/plain');
+//         res.end('Not found');
+//     }
+// });
+
+server.listen(port, function () {
+    console.log('Listening on port', port);
 });
--- pets.js
+++ pets.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
 let pets;
-let command = process.argv[2]
+
 if(process.argv.length < 3) {
     console.error(new Error('Usage: node pets.js [read | create | update | destroy]'))
 }
-
-
+console.log(process.argv)
+let command = process.argv[2]
  fs.readFile('./pets.json', 'utf8' , (error,data) =>{
     if(error) {
          console.error(new Error('Usage: node pets.js [read | create | update | destroy]'))
      } else  {
         pets =  JSON.parse(data)
+      
        
          for(let i = 0; i < pets.length; i++ ) {
             if(command == 'read') {
@@ -22,17 +23,24 @@ if(process.argv.length < 3) {
                  break;
         } 
          }
+          
+
          
-        let pets2 =  {age: Number(process.argv[3]), kind: process.argv[4], name: process.argv[5]}
-        pets.push(pets2)
+    //      if(command = "create") {
+    //     let pets2 =  {age: Number(process.argv[2]), kind: process.argv[4], name: process.argv[5]}
+    //     pets.push(pets2)
     
-        fs.writeFile("./pets.json", JSON.stringify(pets),(err, data) => {
-            if(command == 'create') {
-          console.log(pets)
-        }})
-        module.exports = pets
-      }})
-     
+    //     fs.writeFile("./pets.json", JSON.stringify(pets),(err, data) => {
+    //         if(command == 'create') {
+    //       console.log(pets)
+    // //     }})}
+     module.exports = pets
+     }
+      })
+
+
+
+
 
 
 
